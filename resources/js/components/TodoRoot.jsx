10cbd817e0efc9fcf811dcdbf2300f461a621d44
atsuki20 <atsuki31100220@gmail.com>
2022-09-29 00:07:25 +0900

    update files

--- resources/js/components/TodoRoot.jsx
+++ resources/js/components/TodoRoot.jsx
@@ -1,47 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-import List from './todo/List'
-import NewItem from './todo/NewItem'
+import List from "./todo/List";
+import NewItem from "./todo/NewItem";
 
-import style from './TodoRoot.module.css'
+import style from "./TodoRoot.module.css";
 
 function TodoRoot() {
-  const [items, setItems] = useState([])
+    const [items, setItems] = useState([]);
 
-  const fetchItems = async () => {
-    try {
-      const { headers, data } = await axios.get('/api/todos/')
-      setItems(data)
-      // console.log('request headers', headers)
-    } catch (err) {
-      console.error('request error', err.request.headers)
-    }
-  }
+    const fetchItems = async () => {
+        try {
+            const { headers, data } = await axios.get("/api/todos");
+            setItems(data);
+            // console.log('request headers', headers)
+        } catch (err) {
+            console.error("request error", err.request.headers);
+        }
+    };
 
-  useEffect(() => {
-    fetchItems()
-  }, [])
+    useEffect(() => {
+        fetchItems();
+    }, []);
 
-  const onUpdated = () => {
-    fetchItems()
-  }
+    const onUpdated = () => {
+        fetchItems();
+    };
 
-  return (
-    <div className={style.container}>
-      <div className={style.row}>
-        <h3> Do To </h3>
-        <NewItem onUpdated={() => onUpdated()}></NewItem>
-        <List items={items} onUpdated={() => onUpdated()}></List>
-      </div>
-    </div>
-  )
+    return (
+        <div className={style.container}>
+            <div className={style.row}>
+                <h3> Do To </h3>
+                <NewItem onUpdated={() => onUpdated()}></NewItem>
+                <List items={items} onUpdated={() => onUpdated()}></List>
+            </div>
+        </div>
+    );
 }
 
-export default TodoRoot
+export default TodoRoot;
 
-if (document.getElementById('todo-main')) {
-  ReactDOM.render(<TodoRoot />, document.getElementById('todo-main'))
+if (document.getElementById("todo-main")) {
+    ReactDOM.render(<TodoRoot />, document.getElementById("todo-main"));
 }
